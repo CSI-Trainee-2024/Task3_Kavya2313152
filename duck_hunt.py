ac5b4d96f86b68267b5b0e5a06a6abc772e2f91b
Kavyagarg11 <kcoding4@gmail.com>
2024-10-21 02:45:08 +0530

    changes

--- duck_hunt.py
+++ duck_hunt.py
@@ -14,3 +14,17 @@ screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Duck Hunt")
 clock = pygame.time.Clock()
 duck_image = pygame.image.load('duck.png')
+
+class Duck:
+    def __init__(self):
+        self.image = duck_image
+        self.reset()
+
+    def draw(self, surface):
+        if self.is_visible:
+            surface.blit(self.image, (self.x, self.y))
+
+    def reset(self):
+        self.x = random.randint(0, WIDTH - self.image.get_width())
+        self.y = random.randint(50, HEIGHT // 2)
+        self.is_visible = True
