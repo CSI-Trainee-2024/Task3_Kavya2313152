8d2e5e5da82e37bc50d1c3642c98ecf5118fccfb
Kavyagarg11 <kcoding4@gmail.com>
2024-10-21 02:25:31 +0530

    changes

--- duck_hunt.py
+++ duck_hunt.py
@@ -9,3 +9,7 @@ FPS = 60
 BLUE = (135, 206, 235) 
 BLACK = (0, 0, 0)
 MAX_MISSES = 3 
+
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Duck Hunt")
+clock = pygame.time.Clock()
